3fea1b656eb3b8937b89925bbe5c7921730fe66e
Igor <aaaa@bbb>
2024-03-25 11:39:42 +0300

    Add file kvur.py

--- kvur.py
+++ kvur.py
@@ -1,1 +1,15 @@
+import math
+
 print("Квадратное уравнение.")
+bad_data = True
+while bad_data == True:
+    try:
+        a = int(input("Введите a: "))
+        b = int(input("Введите b: "))
+        c = int(input("Введите c: "))
+        bad_data = False
+    except ValueError:
+        print("value error")
+
+D = b*b - (4*a*c)
+print(D)
